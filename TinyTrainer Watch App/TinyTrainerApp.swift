511c08ade4637820542cd2071246a35c4c9bf95d
nenaodriscoll <nena96@live.nl>
2025-05-01 16:28:39 +0200

    Initial Commit

---
+++ TinyTrainer Watch App/TinyTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTrainerApp.swift
+//  TinyTrainer Watch App
+//
+//  Created by Nena O'Driscoll on 01.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TinyTrainer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinyTrainer Watch AppTests/TinyTrainer_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTrainer_Watch_AppTests.swift
+//  TinyTrainer Watch AppTests
+//
+//  Created by Nena O'Driscoll on 01.05.2025.
+//
+
+import Testing
+@testable import TinyTrainer_Watch_App
+
+struct TinyTrainer_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
